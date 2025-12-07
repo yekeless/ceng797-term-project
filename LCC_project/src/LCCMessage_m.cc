2f9dd3f46e151a3747bc70f94f2469fcd3823c0b
yunus emre keleş <116894704+yekeless@users.noreply.github.com>
2025-12-07 15:23:36 +0300

    Add LccData class and its descriptor

--- LCC_project/src/LCCMessage_m.cc
+++ LCC_project/src/LCCMessage_m.cc
@@ -561,6 +561,428 @@ void LccBeaconDescriptor::setFieldStructValuePointer(omnetpp::any_ptr object, in
     }
 }
 
+Register_Class(LccData)
+
+LccData::LccData() : ::inet::FieldsChunk()
+{
+}
+
+LccData::LccData(const LccData& other) : ::inet::FieldsChunk(other)
+{
+    copy(other);
+}
+
+LccData::~LccData()
+{
+}
+
+LccData& LccData::operator=(const LccData& other)
+{
+    if (this == &other) return *this;
+    ::inet::FieldsChunk::operator=(other);
+    copy(other);
+    return *this;
+}
+
+void LccData::copy(const LccData& other)
+{
+    this->srcId = other.srcId;
+    this->destId = other.destId;
+    this->sendTime = other.sendTime;
+    this->sequenceNumber = other.sequenceNumber;
+}
+
+void LccData::parsimPack(omnetpp::cCommBuffer *b) const
+{
+    ::inet::FieldsChunk::parsimPack(b);
+    doParsimPacking(b,this->srcId);
+    doParsimPacking(b,this->destId);
+    doParsimPacking(b,this->sendTime);
+    doParsimPacking(b,this->sequenceNumber);
+}
+
+void LccData::parsimUnpack(omnetpp::cCommBuffer *b)
+{
+    ::inet::FieldsChunk::parsimUnpack(b);
+    doParsimUnpacking(b,this->srcId);
+    doParsimUnpacking(b,this->destId);
+    doParsimUnpacking(b,this->sendTime);
+    doParsimUnpacking(b,this->sequenceNumber);
+}
+
+int LccData::getSrcId() const
+{
+    return this->srcId;
+}
+
+void LccData::setSrcId(int srcId)
+{
+    handleChange();
+    this->srcId = srcId;
+}
+
+int LccData::getDestId() const
+{
+    return this->destId;
+}
+
+void LccData::setDestId(int destId)
+{
+    handleChange();
+    this->destId = destId;
+}
+
+::omnetpp::simtime_t LccData::getSendTime() const
+{
+    return this->sendTime;
+}
+
+void LccData::setSendTime(::omnetpp::simtime_t sendTime)
+{
+    handleChange();
+    this->sendTime = sendTime;
+}
+
+int LccData::getSequenceNumber() const
+{
+    return this->sequenceNumber;
+}
+
+void LccData::setSequenceNumber(int sequenceNumber)
+{
+    handleChange();
+    this->sequenceNumber = sequenceNumber;
+}
+
+class LccDataDescriptor : public omnetpp::cClassDescriptor
+{
+  private:
+    mutable const char **propertyNames;
+    enum FieldConstants {
+        FIELD_srcId,
+        FIELD_destId,
+        FIELD_sendTime,
+        FIELD_sequenceNumber,
+    };
+  public:
+    LccDataDescriptor();
+    virtual ~LccDataDescriptor();
+
+    virtual bool doesSupport(omnetpp::cObject *obj) const override;
+    virtual const char **getPropertyNames() const override;
+    virtual const char *getProperty(const char *propertyName) const override;
+    virtual int getFieldCount() const override;
+    virtual const char *getFieldName(int field) const override;
+    virtual int findField(const char *fieldName) const override;
+    virtual unsigned int getFieldTypeFlags(int field) const override;
+    virtual const char *getFieldTypeString(int field) const override;
+    virtual const char **getFieldPropertyNames(int field) const override;
+    virtual const char *getFieldProperty(int field, const char *propertyName) const override;
+    virtual int getFieldArraySize(omnetpp::any_ptr object, int field) const override;
+    virtual void setFieldArraySize(omnetpp::any_ptr object, int field, int size) const override;
+
+    virtual const char *getFieldDynamicTypeString(omnetpp::any_ptr object, int field, int i) const override;
+    virtual std::string getFieldValueAsString(omnetpp::any_ptr object, int field, int i) const override;
+    virtual void setFieldValueAsString(omnetpp::any_ptr object, int field, int i, const char *value) const override;
+    virtual omnetpp::cValue getFieldValue(omnetpp::any_ptr object, int field, int i) const override;
+    virtual void setFieldValue(omnetpp::any_ptr object, int field, int i, const omnetpp::cValue& value) const override;
+
+    virtual const char *getFieldStructName(int field) const override;
+    virtual omnetpp::any_ptr getFieldStructValuePointer(omnetpp::any_ptr object, int field, int i) const override;
+    virtual void setFieldStructValuePointer(omnetpp::any_ptr object, int field, int i, omnetpp::any_ptr ptr) const override;
+};
+
+Register_ClassDescriptor(LccDataDescriptor)
+
+LccDataDescriptor::LccDataDescriptor() : omnetpp::cClassDescriptor(omnetpp::opp_typename(typeid(inet::LccData)), "inet::FieldsChunk")
+{
+    propertyNames = nullptr;
+}
+
+LccDataDescriptor::~LccDataDescriptor()
+{
+    delete[] propertyNames;
+}
+
+bool LccDataDescriptor::doesSupport(omnetpp::cObject *obj) const
+{
+    return dynamic_cast<LccData *>(obj)!=nullptr;
+}
+
+const char **LccDataDescriptor::getPropertyNames() const
+{
+    if (!propertyNames) {
+        static const char *names[] = {  nullptr };
+        omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+        const char **baseNames = base ? base->getPropertyNames() : nullptr;
+        propertyNames = mergeLists(baseNames, names);
+    }
+    return propertyNames;
+}
+
+const char *LccDataDescriptor::getProperty(const char *propertyName) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    return base ? base->getProperty(propertyName) : nullptr;
+}
+
+int LccDataDescriptor::getFieldCount() const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    return base ? 4+base->getFieldCount() : 4;
+}
+
+unsigned int LccDataDescriptor::getFieldTypeFlags(int field) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldTypeFlags(field);
+        field -= base->getFieldCount();
+    }
+    static unsigned int fieldTypeFlags[] = {
+        FD_ISEDITABLE,    // FIELD_srcId
+        FD_ISEDITABLE,    // FIELD_destId
+        FD_ISEDITABLE,    // FIELD_sendTime
+        FD_ISEDITABLE,    // FIELD_sequenceNumber
+    };
+    return (field >= 0 && field < 4) ? fieldTypeFlags[field] : 0;
+}
+
+const char *LccDataDescriptor::getFieldName(int field) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldName(field);
+        field -= base->getFieldCount();
+    }
+    static const char *fieldNames[] = {
+        "srcId",
+        "destId",
+        "sendTime",
+        "sequenceNumber",
+    };
+    return (field >= 0 && field < 4) ? fieldNames[field] : nullptr;
+}
+
+int LccDataDescriptor::findField(const char *fieldName) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    int baseIndex = base ? base->getFieldCount() : 0;
+    if (strcmp(fieldName, "srcId") == 0) return baseIndex + 0;
+    if (strcmp(fieldName, "destId") == 0) return baseIndex + 1;
+    if (strcmp(fieldName, "sendTime") == 0) return baseIndex + 2;
+    if (strcmp(fieldName, "sequenceNumber") == 0) return baseIndex + 3;
+    return base ? base->findField(fieldName) : -1;
+}
+
+const char *LccDataDescriptor::getFieldTypeString(int field) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldTypeString(field);
+        field -= base->getFieldCount();
+    }
+    static const char *fieldTypeStrings[] = {
+        "int",    // FIELD_srcId
+        "int",    // FIELD_destId
+        "omnetpp::simtime_t",    // FIELD_sendTime
+        "int",    // FIELD_sequenceNumber
+    };
+    return (field >= 0 && field < 4) ? fieldTypeStrings[field] : nullptr;
+}
+
+const char **LccDataDescriptor::getFieldPropertyNames(int field) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldPropertyNames(field);
+        field -= base->getFieldCount();
+    }
+    switch (field) {
+        default: return nullptr;
+    }
+}
+
+const char *LccDataDescriptor::getFieldProperty(int field, const char *propertyName) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldProperty(field, propertyName);
+        field -= base->getFieldCount();
+    }
+    switch (field) {
+        default: return nullptr;
+    }
+}
+
+int LccDataDescriptor::getFieldArraySize(omnetpp::any_ptr object, int field) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldArraySize(object, field);
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        default: return 0;
+    }
+}
+
+void LccDataDescriptor::setFieldArraySize(omnetpp::any_ptr object, int field, int size) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount()){
+            base->setFieldArraySize(object, field, size);
+            return;
+        }
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        default: throw omnetpp::cRuntimeError("Cannot set array size of field %d of class 'LccData'", field);
+    }
+}
+
+const char *LccDataDescriptor::getFieldDynamicTypeString(omnetpp::any_ptr object, int field, int i) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldDynamicTypeString(object,field,i);
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        default: return nullptr;
+    }
+}
+
+std::string LccDataDescriptor::getFieldValueAsString(omnetpp::any_ptr object, int field, int i) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldValueAsString(object,field,i);
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        case FIELD_srcId: return long2string(pp->getSrcId());
+        case FIELD_destId: return long2string(pp->getDestId());
+        case FIELD_sendTime: return simtime2string(pp->getSendTime());
+        case FIELD_sequenceNumber: return long2string(pp->getSequenceNumber());
+        default: return "";
+    }
+}
+
+void LccDataDescriptor::setFieldValueAsString(omnetpp::any_ptr object, int field, int i, const char *value) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount()){
+            base->setFieldValueAsString(object, field, i, value);
+            return;
+        }
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        case FIELD_srcId: pp->setSrcId(string2long(value)); break;
+        case FIELD_destId: pp->setDestId(string2long(value)); break;
+        case FIELD_sendTime: pp->setSendTime(string2simtime(value)); break;
+        case FIELD_sequenceNumber: pp->setSequenceNumber(string2long(value)); break;
+        default: throw omnetpp::cRuntimeError("Cannot set field %d of class 'LccData'", field);
+    }
+}
+
+omnetpp::cValue LccDataDescriptor::getFieldValue(omnetpp::any_ptr object, int field, int i) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldValue(object,field,i);
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        case FIELD_srcId: return pp->getSrcId();
+        case FIELD_destId: return pp->getDestId();
+        case FIELD_sendTime: return pp->getSendTime().dbl();
+        case FIELD_sequenceNumber: return pp->getSequenceNumber();
+        default: throw omnetpp::cRuntimeError("Cannot return field %d of class 'LccData' as cValue -- field index out of range?", field);
+    }
+}
+
+void LccDataDescriptor::setFieldValue(omnetpp::any_ptr object, int field, int i, const omnetpp::cValue& value) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount()){
+            base->setFieldValue(object, field, i, value);
+            return;
+        }
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        case FIELD_srcId: pp->setSrcId(omnetpp::checked_int_cast<int>(value.intValue())); break;
+        case FIELD_destId: pp->setDestId(omnetpp::checked_int_cast<int>(value.intValue())); break;
+        case FIELD_sendTime: pp->setSendTime(value.doubleValue()); break;
+        case FIELD_sequenceNumber: pp->setSequenceNumber(omnetpp::checked_int_cast<int>(value.intValue())); break;
+        default: throw omnetpp::cRuntimeError("Cannot set field %d of class 'LccData'", field);
+    }
+}
+
+const char *LccDataDescriptor::getFieldStructName(int field) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldStructName(field);
+        field -= base->getFieldCount();
+    }
+    switch (field) {
+        default: return nullptr;
+    };
+}
+
+omnetpp::any_ptr LccDataDescriptor::getFieldStructValuePointer(omnetpp::any_ptr object, int field, int i) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount())
+            return base->getFieldStructValuePointer(object, field, i);
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        default: return omnetpp::any_ptr(nullptr);
+    }
+}
+
+void LccDataDescriptor::setFieldStructValuePointer(omnetpp::any_ptr object, int field, int i, omnetpp::any_ptr ptr) const
+{
+    omnetpp::cClassDescriptor *base = getBaseClassDescriptor();
+    if (base) {
+        if (field < base->getFieldCount()){
+            base->setFieldStructValuePointer(object, field, i, ptr);
+            return;
+        }
+        field -= base->getFieldCount();
+    }
+    LccData *pp = omnetpp::fromAnyPtr<LccData>(object); (void)pp;
+    switch (field) {
+        default: throw omnetpp::cRuntimeError("Cannot set field %d of class 'LccData'", field);
+    }
+}
+
 }  // namespace inet
 
 namespace omnetpp {
